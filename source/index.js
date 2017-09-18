f0278a3b6b7cf4b3d749d8b50319ff9e5f91108f
rogerbf <roger@bergfalk.nu>
2017-09-18 12:04:20 +0200

    initial solution

--- source/index.js
+++ source/index.js
@@ -1,1 +1,31 @@
-export default `fluid-typography`
+import measureTextWidth from "./measure-text-width"
+import calculateRatio from "./calculate-ratio"
+
+export const setFontSize = ({ element = document.createElement(`div`), size = 0 }) =>
+window.requestAnimationFrame(() => {
+  element.style.fontSize = `${size}px`
+})
+
+export const calculateFontSize = (
+  {
+    container = document.createElement(`div`),
+    text = container.textContent.trim()
+  } = {}
+) => {
+  const { width, height } = container
+  const { fontFamily, fontSize } = window.getComputedStyle(container)
+
+  return (
+    parseInt(fontSize) *
+    calculateRatio(
+      container.offsetWidth,
+      measureTextWidth({
+        width,
+        height,
+        fontFamily,
+        fontSize,
+        text
+      })
+    )
+  )
+}
