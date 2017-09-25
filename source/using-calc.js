1949cc2c7c9dbebbfbce79c3e20d9e0d5bb482c8
rogerbf <roger@bergfalk.nu>
2017-09-25 11:37:15 +0200

    fix erroneous font size calculation

--- source/using-calc.js
+++ source/using-calc.js
@@ -1,3 +1,5 @@
+// font-size: calc([minimum size] + ([maximum size] - [minimum size]) * ((100vw - [minimum viewport width]) / ([maximum viewport width] - [minimum viewport width])))
+// font-size: calc(14px + (26 - 14) * ((100vw - 300px) / (1600 - 300)))
 export default (
   {
     fontSize = {
@@ -10,7 +12,4 @@ export default (
     }
   } = {}
 ) =>
-  `calc(${fontSize.min +
-    (fontSize.max -
-      fontSize.min)} * ((100vw - ${viewport.min}px) / ${viewport.max -
-    viewport.min}px))`
+  `calc(${fontSize.min}px + ${(fontSize.max - fontSize.min)} * ((100vw - ${viewport.min}px) / ${viewport.max - viewport.min}))`
